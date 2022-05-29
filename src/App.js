0e6cabc3a4fd819fbfef1ea92c0bf776ae21cb6f
alvin.panganiban <email.of.alvin@gmail.com>
2022-05-29 12:26:25 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+import Cookies from 'universal-cookie';
+
+
 
 function App() {
+
+  axios.get("http://localhost:3001/read", {withCredentials: true}).then((res) => {   // change this to api auth tester.
+  console.log("authorizer OK! :", res.status);
+
+  })
+  .catch(function (error) {
+    console.log("error", error.response.status);
+
+    if( error.response.status== 401){
+
+      const cookies = new Cookies();
+      const currentUrl = window.location.href;
+      cookies.set('CALLBACK_URL', currentUrl, { path: '/' });
+      window.location.replace("http://localhost:3001/sso"); // change this to bizprac sso url
+    }
+  });
+
+
+
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          authorizer test page.
         </p>
         <a
           className="App-link"
@@ -15,7 +40,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+       
         </a>
       </header>
     </div>
